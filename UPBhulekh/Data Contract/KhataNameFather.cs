395a39f131d39535c67bda96df639ccd19ad871f
niqgupta <nikhil.gupta.kumar@gmail.com>
2022-06-18 10:41:50 +0530

    Adding post call to fetch khata details

---
+++ UPBhulekh/Data Contract/KhataNameFather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace UPBhulekh.Data_Contract
+{
+    public class KhataNameFather
+    {
+        public string? khata_number;
+        public string? name;
+        public string? father;
+    }
+}
--- UPBhulekh/Program.cs
+++ UPBhulekh/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using UPBhulekh.LookForName;
+
+Console.WriteLine("Starting now .. ");
+
+string name = "अशोक";
+List<string> vccs = new List<string> { "191191" };
+UsingVCC_Gram khataDetails = new UsingVCC_Gram();
+khataDetails.Fetch(name, vccs);
+foreach (string khataNumber in khataDetails.LookFather_KhataNo(name))
+    Console.WriteLine(khataNumber);
+
+Console.WriteLine("Completed !!");
